61c43851fbd4626af7a708b27b96fa11d73e4b25
Tony Hunter <thunter1987@yahoo.com>
2023-12-03 23:49:53 -0600

    config: disables React devtools in production

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { disableReactDevTools } from "@fvilers/disable-react-devtools"
+import App from "./components/App/App"
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools()
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
-);
+)
